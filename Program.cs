ee9c054d5640adb058bdbc89dd9afcbdb207dcb9
Olarewaju34 <mohmzyo@gmail.com>
2022-06-20 00:20:17 +0100

    loop

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+// int numbers  = 10;
+
+// while (numbers >= 1)
+// {
+//     Console.WriteLine(numbers);
+    
+//  for (int i = 0; i <= 20; i++)
+//  {
+//     double result = Math.Pow(2,i);
+//     Console.WriteLine(result);
+//     i++;
+// //  }
+
+// Random numberGen = new Random();
+
+// int roll1 = 0;
+// int roll2 = 0;
+// int attempts = 0;
+// Console.Write("press enter to roll ");
+
+// while (roll1 != 6 || roll2 != 6)
+// {
+//     Console.ReadKey();
+// roll1 = numberGen.Next(1,7);
+
+// roll2 = numberGen.Next(1,7);
+
+// attempts++;
+// Console.WriteLine($"roll 1 = {roll1}");
+// Console.WriteLine($"roll 2  = {roll2}");
+// }
+
+// Console.WriteLine($"it took {attempts}  attempts to roll number of the same kind");
